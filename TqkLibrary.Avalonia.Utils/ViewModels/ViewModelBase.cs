ab2fcab3dee7033babcb9da32a368ea9fa1e0f9c
tqk2811 <tqk2811@gmail.com>
2025-11-27 17:42:37 +0700

    add ViewModelBase with INotifyDataErrorInfo

---
+++ TqkLibrary.Avalonia.Utils/ViewModels/ViewModelBase.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections;
+using System.ComponentModel;
+
+namespace TqkLibrary.Avalonia.Utils.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject, INotifyDataErrorInfo
+    {
+        #region INotifyDataErrorInfo
+        public bool HasErrors => _errors.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return propertyName != null && _errors.TryGetValue(propertyName, out var list)
+                ? list
+                : Enumerable.Empty<string>();
+        }
+        #endregion
+
+        private readonly Dictionary<string, List<string>> _errors = new();
+        protected void AddError(string prop, string error)
+        {
+            _errors[prop] = new List<string>() { error };
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(prop));
+        }
+        protected void AddError(string prop, IEnumerable<string> errors)
+        {
+            _errors[prop] = [.. errors];
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(prop));
+        }
+        protected void ClearErrors(string prop)
+        {
+            if (_errors.Remove(prop))
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(prop));
+        }
+        protected void ClearErrors()
+        {
+            var keys = _errors.Keys.ToList();
+            _errors.Clear();
+            foreach (var key in keys)
+            {
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(key));
+            }
+        }
+    }
+}
