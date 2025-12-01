7940763f4a62bdbbfd1f86529c75aa2d65945089
tqk2811 <tqk2811@gmail.com>
2025-12-01 15:42:01 +0700

    add SetClipboardCommand

---
+++ TqkLibrary.Avalonia.ToolKit/Commands/SetClipboardCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TqkLibrary.Avalonia.ToolKit.Interfaces.Services;
+
+namespace TqkLibrary.Avalonia.ToolKit.Commands
+{
+    public class SetClipboardCommand : BaseCommand
+    {
+        readonly IClipboardService _clipboardService;
+        public SetClipboardCommand(
+            IClipboardService clipboardService
+            )
+        {
+            this._clipboardService = clipboardService;
+        }
+
+        public override async void Execute(object? parameter)
+        {
+            string? text = parameter as string;
+            if (string.IsNullOrWhiteSpace(text))
+                text = parameter?.ToString();
+            if (string.IsNullOrWhiteSpace(text))
+                return;
+
+            using var l = LockButton();
+
+            var Permission = await _clipboardService.HasPermissionAsync();
+            if (Permission.Read != true)
+                Permission = await _clipboardService.RequestPermissionAsync(new() { Write = true });
+            if (Permission.Read == true)
+            {
+                await _clipboardService.SetTextAsync(text!);
+            }
+        }
+    }
+}
